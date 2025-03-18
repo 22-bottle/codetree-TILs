ee112aeeadcfa9f0ce5c36e4b3b37b8f7de02aad
이희병 <101461544+22-bottle@users.noreply.github.com>
2025-03-18 14:16:49 +0900

    Title: 바이러스 검사, Time: 96ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250318/바이러스 검사/virus-detector.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N, jang, won;
+    static int[] rests;
+    
+    public static void main(String[] args) throws Exception {
+        init();
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer++;
+            rests[i] -= jang;
+            if (rests[i] <= 0) continue;
+            answer += rests[i] / won + 1;
+        }
+        System.out.println(answer);
+    }
+
+    public static void init() throws Exception {
+        N = Integer.parseInt(br.readLine());
+        rests = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            rests[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        jang = Integer.parseInt(st.nextToken());
+        won = Integer.parseInt(st.nextToken());
+    }
+}
